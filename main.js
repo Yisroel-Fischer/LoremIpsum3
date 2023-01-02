23c04dba5e11ca1a8a6d76cfd88dac19b0fd4643
Yisroel-Fischer <ymfischer3@gmail,com>
2023-01-02 06:50:36 -0500

    vscode commit, addition of portrait media query

---
+++ main.js
@@ -0,0 +1,15 @@
+const clearIcon = document.querySelector("#clear");
+  const searchBar = document.querySelector("#searchbar");
+
+  searchBar.addEventListener("keyup", () => {
+    if(searchBar.value && clearIcon.style.visibility != "visible"){
+      clearIcon.style.visibility = "visible";
+    } else if(!searchBar.value) {
+      clearIcon.style.visibility = "hidden";
+    }
+  });
+
+  clearIcon.addEventListener("click", () => {
+    searchBar.value = "";
+    clearIcon.style.visibility = "hidden";
+  })
