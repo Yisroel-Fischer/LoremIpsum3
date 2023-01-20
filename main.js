fad579c82e79db8f191ac5046e98cf50918ebe35
Yisroel-Fischer <ymfischer3@gmail,com>
2023-01-19 20:31:29 -0500

    hamburger menu

---
+++ main.js
@@ -0,0 +1,8 @@
+function dropdownMenu() {
+    var x = document.getElementById("hamburger-menu");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
